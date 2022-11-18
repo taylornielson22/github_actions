6dfa0628312750b7e304fb92fb1e4bc247305246
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-17 23:39:55 -0700

    try again

---
+++ .github/scripts/bumpv.sh
@@ -0,0 +1,20 @@
+set -x
+
+echo "performing git setting"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+git config --global user.name "GitHub Actions"
+git config --global user.email $GIT_EMAIL
+git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git 
+
+git fetch origin 
+
+echo "Updating patch version on master branch"
+git checkout main
+git merge --strategy-option ours origin/prod
+
+#bump2version patch
+echo "hello" > hi.txt
+git add hi.txt
+git commit -m "test"
+
+git push origin main
--- .github/scripts/merge2main.sh
+++ .github/scripts/merge2main.sh
@@ -9,23 +9,11 @@ git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git
 echo "fetching tags"
 git fetch --tags origin
 
-echo "update master if needed"
-git checkout -f main
-git merge --strategy-option ours origin/prod
-git push origin main
-
 echo "checking out prod"
-git checkout prod
+git checkout -f prod
 
 echo "pushing changes from qa-accept sha to production"
 git merge qa-accept --no-ff -m 'Automated Merge Process: Triggered by qa-accept tag on master'
 git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git push origin prod
 
-echo "Updating patch version on master branch"
-git checkout main
-#bump2version patch
-echo "hello" > hi.txt
-git commit hi.txt -m "test"
-
-git push -u origin main
