7bbed056d28604f521cf49f1ff4d44681205d79e
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-18 14:58:31 -0700

    try again

---
+++ .github/scripts/merge.sh
@@ -0,0 +1,18 @@
+set -x
+
+echo "performing git setting"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+git config --global user.name "GitHub Actions"
+git config --global user.email $GIT_EMAIL
+git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git 
+
+echo "fetching tags"
+git fetch --tags origin
+
+echo "checking out prod"
+git checkout -f prod
+
+echo "pushing changes from qa-accept sha to production"
+git merge qa-accept --no-ff -m 'Automated Merge Process: Triggered by qa-accept tag on master'
+git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git push origin prod
--- .github/scripts/merge2main.sh
+++ .github/scripts/merge2main.sh
@@ -6,19 +6,13 @@ git config --global user.name "GitHub Actions"
 git config --global user.email $GIT_EMAIL
 git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git 
 
-echo "fetching tags"
-git fetch --tags origin
+git fetch origin 
+
+
 
 echo "Updating patch version on master branch"
 git checkout -f main
-git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git merge origin/prod
+git merge --strategy-option ours origin/prod
 git push origin main
 
-echo "checking out prod"
-git checkout -f prod
 
-echo "pushing changes from qa-accept sha to production"
-git merge qa-accept --no-ff -m 'Automated Merge Process: Triggered by qa-accept tag on master'
-git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git push origin prod
