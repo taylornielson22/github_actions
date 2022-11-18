a7e7b68c175a55592112991069f4d26f04fee67a
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-17 21:18:09 -0700

    try again

--- .github/scripts/merge2main.sh
+++ .github/scripts/merge2main.sh
@@ -6,14 +6,10 @@ git config --global user.name "GitHub Actions"
 git config --global user.email $GIT_EMAIL
 git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git 
 echo "fetching tags"
-git fetch --tags origin
-
-
-echo "checking out production "
-git checkout -B origin/prod origin/prod
+# git fetch --tags origin
 
 echo "pushing changes from qa-accept sha to production"
 git merge qa-accept --no-ff -m 'Automated Merge Process: Triggered by qa-accept tag on master'
 git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git push --force-with-lease origin origin/prod:prod
+git push origin prod
 
