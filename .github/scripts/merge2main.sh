605e9665c5bc0951c9e69c451ad6cf390d1534fd
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-17 17:13:22 -0700

    add script

---
+++ .github/scripts/merge2main.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+# This script acts as an entrypoint to cfl-doxygen
+set -x
+export GIT_TOKEN="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"
+export GIT_EMAIL="nielson.taylorm@gmail.com"
+
+echo "performing git setting"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
+git config --global user.name "GitHub Actions"
+git config --global user.email $GIT_EMAIL
+git remote add origin https://token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git
+
+echo "fetching tags"
+git fetch --tags origin
+
+echo "checking out production "
+git checkout --track origin/prod
+
+echo "pushing changes from qa-accept sha to production"
+git merge qa-accept --no-ff -m 'Automated Merge Process: staging merge to production'
+git remote set-url --push origin "https://token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git"
+git push origin prod
+
+
+echo "Updating patch version on master branch"
+git checkout origin main
+echo "hi" > hi.txt
+git push https://token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git
