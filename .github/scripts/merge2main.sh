d3e2a84aedfe66f4fecc25891eb7a675b6c04357
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-17 21:42:05 -0700

    try again

--- .github/scripts/merge2main.sh
+++ .github/scripts/merge2main.sh
@@ -14,3 +14,9 @@ echo "pushing changes from qa-accept sha to production"
 git merge qa-accept --no-ff -m 'Automated Merge Process: Triggered by qa-accept tag on master'
 git remote set-url --push origin "https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git push origin prod
+
+echo "Updating patch version on master branch"
+git checkout origin main
+#bump2version patch
+echo "hello" > hi.txt
+git push origin main
