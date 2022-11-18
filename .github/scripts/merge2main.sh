eeaf6225d5cd668e4e7d68d513518efb74f5de81
Taylor Nielson <nielson.taylorm@gmail.com>
2022-11-17 17:51:15 -0700

    fix

--- .github/scripts/merge2main.sh
+++ .github/scripts/merge2main.sh
@@ -7,19 +7,19 @@ echo "performing git setting"
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global user.name "GitHub Actions"
 git config --global user.email $GIT_EMAIL
-git remote add origin https://token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git
-
+git remote set-url origin https://x-access-token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git
+git remote add fork https://x-access-token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git
 
 echo "fetching tags"
 git fetch --tags origin
+git fetch fork prod
 
 echo "checking out production "
-git checkout -f origin/prod
+git checkout -b fork/prod fork/prod
 
 echo "pushing changes from qa-accept sha to production"
-git merge qa-accept --no-ff -m 'Automated Merge Process: staging merge to production'
-git remote set-url --push origin "https://token:$GIT_TOKEN@github.hpe.com/$GITHUB_REPOSITORY.git"
-git push origin prod
+git merge qa-accept -m 'Automated Merge Process: staging merge to production'
+git push --force-with-lease fork fork/prod:prod
 
 
 echo "Updating patch version on master branch"
