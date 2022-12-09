aabb4b8dbb971411c44344e0a4ce0522bca43033
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2022-12-09 12:20:28 -0700

    Update bumpv.sh

--- .github/scripts/bumpv.sh
+++ .github/scripts/bumpv.sh
@@ -3,14 +3,14 @@ set -x
 echo "performing git setting"
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global user.name "GitHub Actions"
-git config --global user.email $GIT_EMAIL
+git config --global user.email "$GIT_EMAIL"
 git remote add origin https://token:$GIT_TOKEN@github.com/$GITHUB_REPOSITORY.git 
 
 echo "Updating patch version on master branch"
 git checkout -f master
 echo "$VER_TYPE"
 git push -f origin master
-
+echo "$GIT_EMAIL"
 #bump2version patch
 touch bump.txt
 git add bump.txt
