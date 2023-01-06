feed281f369c7f7aba6e30c2bf86738fca0ca4b1
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2023-01-06 16:03:08 -0700

    Update create_release_slack_msg.sh

--- .github/scripts/create_release_slack_msg.sh
+++ .github/scripts/create_release_slack_msg.sh
@@ -9,6 +9,7 @@ APPEND_MSG_FILE=.github/release_msg_note.txt
 echo "*New ${PACKAGE_NAME} package v${VERSION} released!*" > $SLACK_MSG_CONTENTS
 echo "=======================================" >> $SLACK_MSG_CONTENTS
 echo "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/releases/tag/${VERSION}" >> $SLACK_MSG_CONTENTS
+echo "\n\n*Changes*" >> $SLACK_MSG_CONTENTS
 
 # Grab Release Notes from latest release
 RELEASE_API_URL="${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/tags/${VERSION}"
@@ -20,7 +21,7 @@ NOTES=$(echo "$NOTES" | sed '/^[*] /!d' | sed 's/* //g')
 NOTES=$(echo "$NOTES" | sed "s+${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/+#+g")
 
 # Append $NOTES & .github/release_msg_note.txt into $SLACK_MSG_CONTENTS file
-echo "\n\n*Changes*$NOTES"  >> $SLACK_MSG_CONTENTS
+echo "*Changes*$NOTES"  >> $SLACK_MSG_CONTENTS
 cat $APPEND_MSG_FILE >> $SLACK_MSG_CONTENTS
 
 # Set multi-line SLACK_MESSAGE enviroment = text in $SLACK_MSG_CONTENTS file
