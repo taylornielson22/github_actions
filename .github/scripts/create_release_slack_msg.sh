c53c02dba7cd15451b87bd663e85700c6c532a32
taylornielson22 <53064096+taylornielson22@users.noreply.github.com>
2023-01-06 15:28:22 -0700

    Create create_release_slack_msg.sh

---
+++ .github/scripts/create_release_slack_msg.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# Set file where slack message contents will be pasted into
+SLACK_MSG_CONTENTS=slack_msg.txt
+# File that contains message text to append at end of slack message
+APPEND_MSG_FILE=.github/release_msg_note.txt
+
+# Paste Title & Release URL into $SLACK_MSG_CONTENTS file
+TITLE="*New ${PACKAGE_NAME} package v${VERSION} released!*"
+LINE_BREAK="\n=======================================\n"
+RElEASE_SERVER_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/releases/tag/${VERSION}"
+echo "${TITLE}${LINE_BREAK}${RElEASE_SERVER_URL}\n\n*Changes*" > $SLACK_MSG_CONTENTS
+
+# Grab Release Notes from latest release
+RELEASE_API_URL="${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/releases/tags/${VERSION}"
+NOTES=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" "${RELEASE_API_URL}" | jq '.body')
+
+# Remove everything besides list of changes 
+NOTES=$(echo "$NOTES" | sed '/^[*] /!d' | sed 's/* //g')
+# Replace PR URLs with PR #
+NOTES=$(echo "$NOTES" | sed "s+${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/+#+g")
+
+# Append $NOTES & .github/release_msg_note.txt into $SLACK_MSG_CONTENTS file
+echo "$NOTES"  >> $SLACK_MSG_CONTENTS
+cat $APPEND_MSG_FILE >> $SLACK_MSG_CONTENTS
+
+# Set multi-line SLACK_MESSAGE enviroment = text in $SLACK_MSG_CONTENTS file
+echo 'SLACK_MESSAGE<<EOF' >> $GITHUB_ENV
+echo "$(cat $SLACK_MSG_CONTENTS)" >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
